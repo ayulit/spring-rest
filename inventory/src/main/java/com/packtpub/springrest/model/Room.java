7fb00c588e04a0c2419e5a051bed4c592dd4a4e4
ayulit <litwinoff@gmail.com>
2017-03-24 23:02:12 +0300

    toString() implemented + some format fix

--- inventory/src/main/java/com/packtpub/springrest/model/Room.java
+++ inventory/src/main/java/com/packtpub/springrest/model/Room.java
@@ -1,55 +1,60 @@
 package com.packtpub.springrest.model;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
 
-@Entity(name="rooms")
+import javax.persistence.*;
+
+@Entity(name = "rooms")
 public class Room {
 
-	private long id;
-	private RoomCategory roomCategory;
-	private String name;
-	private String description;
-	
-	@Id
-	@GeneratedValue
-	public long getId() {
-		return id;
-	}
-	public void setId(long id) {
-		this.id = id;
-	}
-	
-	/* Many Rooms to One RoomCategory */
-	@ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.REFRESH}, fetch=FetchType.EAGER)
-	public RoomCategory getRoomCategory() {
-		return roomCategory;
-	}
-	public void setRoomCategory(RoomCategory roomCategory) {
-		this.roomCategory = roomCategory;
-	}
-	
-	@Column(name="name", unique=true, nullable=false, length=128)
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	@Column(name="description")
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-	// TODO override toString method
-	
+    private long id;
+    private RoomCategory roomCategory;
+    private String name;
+    private String description;
+
+    @Id
+    @GeneratedValue
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /* Many Rooms to One RoomCategory */
+    @ManyToOne(cascade = { CascadeType.PERSIST, CascadeType.REFRESH }, fetch = FetchType.EAGER)
+    public RoomCategory getRoomCategory() {
+        return roomCategory;
+    }
+
+    public void setRoomCategory(RoomCategory roomCategory) {
+        this.roomCategory = roomCategory;
+    }
+
+    @Column(name = "name", unique = true, nullable = false, length = 128)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        // some kind of building string from reflection of class name ...?
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+    }
+
 }
