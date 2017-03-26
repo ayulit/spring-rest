4e44bc3ebb0147ca7a209f3add238236ebc3cd9f
ayulit <litwinoff@gmail.com>
2017-03-26 23:04:20 +0300

    Some minor format fix

--- inventory/src/main/java/com/packtpub/springrest/inventory/InventoryService.java
+++ inventory/src/main/java/com/packtpub/springrest/inventory/InventoryService.java
@@ -6,14 +6,16 @@ import com.packtpub.springrest.model.Room;
 import com.packtpub.springrest.model.RoomCategory;
 
 public interface InventoryService {
-	
-	public void addRoomCategory(RoomCategory category);
-	public RoomCategory getRoomCategory(long categoryId);
-	
-	public void addRoom(Room room);
-	/* look up a room by its identifier */
-	public Room getRoom(long roomId);
-	
-	public List<Room> getAllRoomsWithCategory(RoomCategory category);
+
+    public void addRoomCategory(RoomCategory category);
+
+    public RoomCategory getRoomCategory(long categoryId);
+
+    public void addRoom(Room room);
+
+    /* look up a room by its identifier */
+    public Room getRoom(long roomId);
+
+    public List<Room> getAllRoomsWithCategory(RoomCategory category);
 
 }
--- inventory/src/main/java/com/packtpub/springrest/inventory/web/RoomsResource.java
+++ inventory/src/main/java/com/packtpub/springrest/inventory/web/RoomsResource.java
@@ -16,32 +16,32 @@ import com.packtpub.springrest.model.RoomCategory;
 @RestController               // we instruct Spring that RoomsResource is a controller = @Component? It will be bean? 
 @RequestMapping("/rooms")     // like @Path in JAX-RS
 public class RoomsResource {  // resource like in JAX-RS : maps request whithin the code
-	
-	// TODO implement injection using @Autowired
-	private final InventoryService inventoryService;
-	public RoomsResource(InventoryService inventoryService) {
-		this.inventoryService = inventoryService;
-	}
-	
-	// retrieving a Room by identifier
-	@RequestMapping(value="/{roomId}", method = RequestMethod.GET)  // like @GET in JAX-RS
-	public RoomDTO getRoom(@PathVariable("roomId") long id) {
-		Room room = inventoryService.getRoom(id);
-		
-		// Data Transfer Object (DTO) pattern:
-		// It provides a useful decoupling between the persistence and presentation layers.
-		return new RoomDTO(room);
-	}
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<RoomDTO> getRoomsInCategory(@RequestParam("categotyId") long categoryId) {
-		RoomCategory category = inventoryService.getRoomCategory(categoryId);
-		
-		return inventoryService.getAllRoomsWithCategory(category)
-							   .stream()                      // Java-8 Stream API
-							   .map(RoomDTO::new)             // Java-8 Method Reference
-							   .collect(Collectors.toList());
-	} 
+
+    // TODO implement injection using @Autowired
+    private final InventoryService inventoryService;
+    public RoomsResource(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    // retrieving a Room by identifier
+    @RequestMapping(value="/{roomId}", method = RequestMethod.GET)  // like @GET in JAX-RS
+    public RoomDTO getRoom(@PathVariable("roomId") long id) {
+        Room room = inventoryService.getRoom(id);
+
+        // Data Transfer Object (DTO) pattern:
+        // It provides a useful decoupling between the persistence and presentation layers.
+        return new RoomDTO(room);
+    }
+
+    @RequestMapping(method=RequestMethod.GET)
+    public List<RoomDTO> getRoomsInCategory(@RequestParam("categotyId") long categoryId) {
+        RoomCategory category = inventoryService.getRoomCategory(categoryId);
+
+        return inventoryService.getAllRoomsWithCategory(category)
+                .stream()                      // Java-8 Stream API
+                .map(RoomDTO::new)             // Java-8 Method Reference
+                .collect(Collectors.toList());
+    } 
 
 
 }
