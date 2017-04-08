1a59b2f9c7dbbbb70e0bdaaeb5ae5a337629c054
alitvin <litwinoff@gmail.com>
2017-04-08 15:10:51 +0300

    ch4: API response format

--- inventory/src/main/java/com/packtpub/springrest/inventory/web/RoomsResource.java
+++ inventory/src/main/java/com/packtpub/springrest/inventory/web/RoomsResource.java
@@ -1,36 +1,39 @@
 package com.packtpub.springrest.inventory.web;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.packtpub.springrest.RecordNotFoundException;
 
 import com.packtpub.springrest.inventory.InventoryService;
+import com.packtpub.springrest.inventory.web.ApiResponse.ApiError;
+import com.packtpub.springrest.inventory.web.ApiResponse.Status;
 import com.packtpub.springrest.model.Room;
 import com.packtpub.springrest.model.RoomCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-@RestController               // we instruct Spring that RoomsResource is a controller = @Component? It will be bean? 
-@RequestMapping("/rooms")     // like @Path in JAX-RS
-public class RoomsResource {  // resource like in JAX-RS : maps request whithin the code
+import java.util.stream.Collectors;
+
+
+
+@RestController  
+@RequestMapping("/rooms") 
+public class RoomsResource { 
 
     @Autowired
     private InventoryService inventoryService;
     
     public RoomsResource() {}
 
-    // retrieving a Room by identifier
-    @RequestMapping(value="/{roomId}", method = RequestMethod.GET)  // like @GET in JAX-RS
-    public RoomDTO getRoom(@PathVariable("roomId") long id) {
-        Room room = inventoryService.getRoom(id);
 
-        // Data Transfer Object (DTO) pattern:
-        // It provides a useful decoupling between the persistence and presentation layers.
-        return new RoomDTO(room);
+    @RequestMapping(value="/{roomId}", method = RequestMethod.GET)
+    public ApiResponse getRoom(@PathVariable("roomId") long id) {
+        try {
+            Room room = inventoryService.getRoom(id);
+            return new ApiResponse(Status.OK, new RoomDTO(room), null);
+        } catch (RecordNotFoundException e) {
+            return new ApiResponse(Status.ERROR, null, new ApiError(999, "No room with ID " + id));
+        }
     }
 
     @RequestMapping(method=RequestMethod.GET)
