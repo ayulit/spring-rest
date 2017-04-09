5fd79f5f51b367927f84e9dd83bd27b9d9856b9a
alitvin <litwinoff@gmail.com>
2017-04-09 19:08:50 +0300

    ch07: The booking service

--- booking/src/main/java/com/packtpub/springrest/booking/BookingService.java
+++ booking/src/main/java/com/packtpub/springrest/booking/BookingService.java
@@ -13,10 +13,28 @@ import java.util.List;
  */
 public interface BookingService {
 
+    /**
+    * Processes the given booking
+    *
+    * @param request the booking request
+    * @return the result of the request
+    */
     public BookingResponse book(BookingRequest request);
 
+    /**
+    * Looks up the booking with the given identifier.
+    *
+    * @param bookingId the booking identifier to look up
+    * @return the booking with the given ID
+    */
     public Booking getBooking(long bookingId);
 
+    /**
+    * Answers all bookings for the given date range.
+    *
+    * @param dateRange the date range to retrieve bookings for
+    * @return the bookings in the given date range
+    */
     public List<Booking> getBookings(DateRange dateRange);
 
     public List<Booking> getBookings(DateRange dateRange, RoomCategory roomCategory);
