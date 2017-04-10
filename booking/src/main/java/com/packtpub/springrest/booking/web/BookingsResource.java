146e02adfc3a0e6ca4fd0b2170f0fe539887fa81
alitvin <litwinoff@gmail.com>
2017-04-10 11:50:58 +0300

    ch07: Booking REST resource

--- booking/src/main/java/com/packtpub/springrest/booking/web/BookingsResource.java
+++ booking/src/main/java/com/packtpub/springrest/booking/web/BookingsResource.java
@@ -1,8 +1,15 @@
 package com.packtpub.springrest.booking.web;
 
 import com.packtpub.springrest.booking.BookingService;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,8 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/bookings")
 public class BookingsResource {
 
+    private static final Logger logger = LoggerFactory.getLogger(BookingsResource.class);
+    
     @Autowired
     private BookingService bookingService;
 
     public BookingsResource() {}
+    
+    @RequestMapping(value = "/{bookingId}", method = RequestMethod.GET)
+    public BookingDTO getBooking(@PathVariable("bookingId") long bookingId) {
+        return new BookingDTO(bookingService.getBooking(bookingId));
+    }    
+    
 }
