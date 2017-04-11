0805176e7196e4b74a9db1e288bc7a80db02fc11
ayulit <litwinoff@gmail.com>
2017-04-11 19:46:29 +0300

    ch07: URL mapping authorization

--- booking/src/main/java/com/packtpub/springrest/booking/web/security/SecurityConfig.java
+++ booking/src/main/java/com/packtpub/springrest/booking/web/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.packtpub.springrest.booking.web.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -39,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
         http.csrf().disable().authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/bookings/**").hasRole("ADMIN")
         .anyRequest().authenticated();
 
         switch (scheme) {
